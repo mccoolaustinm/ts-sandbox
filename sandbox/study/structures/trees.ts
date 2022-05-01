848700134cc1a32f13692644b929d981eb48587f
Austin <24414519+mccoolaustinm@users.noreply.github.com>
2022-05-01 07:29:35 -0400

    bst remove()

--- sandbox/study/structures/trees.ts
+++ sandbox/study/structures/trees.ts
@@ -91,7 +91,7 @@ class BinarySearchTree<Type> {
 
         //? remove and reconnect children to parent
 
-        if (!vertex.right) { // vertex has no right child, simplest scenario
+        if (!vertex.right) { //? vertex has no right child, simplest scenario
 
             if (!parent){ // no parent, vertex is root, pass left (possibly null) child up to root
                 this.root = vertex.left 
@@ -102,7 +102,7 @@ class BinarySearchTree<Type> {
             if (parent.childIsLeft) parent.vertex.left = vertex.left
             else if (parent.childIsRight) parent.vertex.right = vertex.left
 
-        } else if (!vertex.right.left) { // vertex has a right child with no left child
+        } else if (!vertex.right.left) { //? vertex has a right child with no left child
 
             // reconnect vertex's left child to the vertex's right child 
             vertex.right.left = vertex.left
@@ -116,9 +116,46 @@ class BinarySearchTree<Type> {
             if (parent.childIsLeft) parent.vertex.left = vertex.right
             else if (parent.childIsRight) parent.vertex.right = vertex.right
 
-        } else { // vertex has a right child with a left child
-            // todo: no idea
-            throw new Error('i havent figured out how to do this yet!')
+        } else { //? vertex has a right child with a left child
+
+            // if the vertex has a deep right subtree, then we need to find the best candidate to replace the vertex from its right tree
+            // now, we know that every item in a right subtree is larger than the vertex
+            // and every item that is left of the vertex is smaller
+            // so the smallest item in the right subtree is larger than the largest item of the left subtree
+            // if we find the smallest item in the right subtree, that item can replace the vertex
+                // it will be the *leftmost* descendent of the right subtree
+            // we will need to locate the leftmost right-descendent and substitute it for the vertex
+
+            // need to find the right child's leftmost descendent (and its parent, for the sake of moving it)
+            let leftmostParent = vertex.right 
+            let leftmostDescendent = vertex.right.left // traverse the right child's left subtree
+            while (leftmostDescendent.left) { // while we can go further left
+                leftmostParent = leftmostDescendent
+                leftmostDescendent = leftmostParent.left!
+            }
+
+            // move leftmost's right subtree up to leftmost's parent's left (where leftmost itself previously was)
+            // by nature of BST ordering, everything in leftmost's right subtree can still fit as its parent's left subtree
+            leftmostParent.left = leftmostDescendent.right
+            // we went as far left as we could to find leftmost, so by definition leftmost's "left" is empty and nothing needs to be moved
+            leftmostDescendent.left = vertex.left 
+            // and leftmost's right was given up to its parent
+            // now that the leftmost has been removed from the vertex's right subtree, we can be sure that
+                // since the leftmost is the smallest item in the right subtree
+                // everything else in the vertex's right subtree is *larger* than the leftmost and can be valid as its right tree
+            // and since the leftmost was previously in the vertex's right subtree, we can be sure that
+                // everything in the vertex's left subtree is *smaller* than the leftmost and can be valid as its left tree
+            // thus leftmost is valid to replace the vertex we want to remove
+            leftmostDescendent.right = vertex.right 
+
+            if (!parent){ // no parent, vertex is root, so the leftmost that replaced it will also be its root
+                this.root = leftmostDescendent
+                return true
+            }
+
+            // connect right child to parent
+            if (parent.childIsLeft) parent.vertex.left = leftmostDescendent
+            else if (parent.childIsRight) parent.vertex.right = leftmostDescendent
         }
 
         return true
